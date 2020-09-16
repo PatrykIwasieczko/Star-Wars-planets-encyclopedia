94cdeec31a1041130c1b220fffb21512d5b5058f
Patryk <patryk.iwasieczko@gmail.com>
2020-09-16 22:31:47 +0200

    Refactor CollapsibleContent component

--- src/components/CollapsibleContent.tsx
+++ src/components/CollapsibleContent.tsx
@@ -10,27 +10,25 @@ type Props = {
 };
 
 const CollapsibleContent: React.FC<Props> = ({ singlePlanet, isOpen }) => {
-    const {
-        planetName,
-        rotationPeriod,
-        orbitalPeriod,
-        diameter,
-        climate,
-        surfaceWater,
-        population,
-    } = singlePlanet;
+    const headers = [
+        singlePlanet.planetName,
+        singlePlanet.rotationPeriod,
+        singlePlanet.orbitalPeriod,
+        singlePlanet.diameter,
+        singlePlanet.climate,
+        singlePlanet.surfaceWater,
+        singlePlanet.population,
+    ];
     return (
         <ul
             key={singlePlanet.planetId}
             className={isOpen ? "collapsible-opened" : "collapsible-closed"}
         >
-            <p className="collapsible-text text-content">{planetName}</p>
-            <p className="collapsible-text text-content">{rotationPeriod}</p>
-            <p className="collapsible-text text-content">{orbitalPeriod}</p>
-            <p className="collapsible-text text-content">{diameter}</p>
-            <p className="collapsible-text text-content">{climate}</p>
-            <p className="collapsible-text text-content">{surfaceWater}</p>
-            <p className="collapsible-text text-content">{population}</p>
+            {headers.map((header, index) => (
+                <p key={index} className="collapsible-text text-content">
+                    {header}
+                </p>
+            ))}
         </ul>
     );
 };
